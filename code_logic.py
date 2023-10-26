6843b60c5afac42a460e20a2987cb9578aa1424b
himanshu1027 <141421700+himanshu1027@users.noreply.github.com>
2023-10-26 15:16:20 +0530

    Update code_logic.py

--- code_logic.py
+++ code_logic.py
@@ -2,30 +2,33 @@ import re
 import paddleocr
 
 def extract_yolo_output_text_with_paddleocr(output):
-    detections = {}
-    for line in output.splitlines():
-      match = re.match(r"(?P<name>\w+):\s+(?P<percentage>\d+)%\s+\(left_x:\s+(?P<left_x>\d+)\s+top_y:\s+(?P<top_y>\d+)\s+width:\s+(?P<width>\d+)\s+height:\s+(?P<height>\d+)\)", line)
-      if match:
-        class_name = match.group("name")
-        bbox = (match.group("left_x"), match.group("top_y"), match.group("left_x") + match.group("width"), match.group("top_y") + match.group("height"))
-        if class_name not in detections:
-          detections[class_name] = []
-        detections[class_name].append((match.group("percentage"), bbox))
-    # Extract the bounding box with the highest percentage confidence.
-    text_outputs = []
-    for class_name, bounding_boxes in detections.items():
-      highest_percentage = max(bounding_boxes, key=lambda x: x[0])[1]
-      # Crop the image and extract the text from the bounding box using PaddleOCR.
-      label_img = image.crop(highest_percentage)
-      ocr = paddleocr.PaddleOCR(det_lang='ml', use_gpu=False, use_angle_cls=True, det_algorithm="DB")
-      results = ocr.ocr(label_img)
-  
-      # Get the text from the OCR results.
-      if results:
-        text = results[0][0].get("text")
-        text_outputs.append(text)
-  
-    return text_outputs
+    try:
+        detections = {}
+        for line in output.splitlines():
+          match = re.match(r"(?P<name>\w+):\s+(?P<percentage>\d+)%\s+\(left_x:\s+(?P<left_x>\d+)\s+top_y:\s+(?P<top_y>\d+)\s+width:\s+(?P<width>\d+)\s+height:\s+(?P<height>\d+)\)", line)
+          if match:
+            class_name = match.group("name")
+            bbox = (match.group("left_x"), match.group("top_y"), match.group("left_x") + match.group("width"), match.group("top_y") + match.group("height"))
+            if class_name not in detections:
+              detections[class_name] = []
+            detections[class_name].append((match.group("percentage"), bbox))
+        # Extract the bounding box with the highest percentage confidence.
+        text_outputs = []
+        for class_name, bounding_boxes in detections.items():
+          highest_percentage = max(bounding_boxes, key=lambda x: x[0])[1]
+          # Crop the image and extract the text from the bounding box using PaddleOCR.
+          label_img = image.crop(highest_percentage)
+          ocr = paddleocr.PaddleOCR(det_lang='ml', use_gpu=False, use_angle_cls=True, det_algorithm="DB")
+          results = ocr.ocr(label_img)
+      
+          # Get the text from the OCR results.
+          if results:
+            text = results[0][0].get("text")
+            text_outputs.append(text)
+      
+        return text_outputs
+    except Exception as e:
+        logger.info("")
 
 # Extract the text output from the YOLO model prediction using PaddleOCR.
 text_outputs = extract_yolo_output_text_with_paddleocr(s_output)
